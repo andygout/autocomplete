a5d659064cbe489f2c00da49b8a396f3c6081717
Andy Gout <andygout@users.noreply.github.com>
2026-03-03 17:05:52 +0000

    Remove demos + stories (#3)

--- demos/src/static/static.js
+++
@@ -1,6 +0,0 @@
-import '../../../main.js';
-import oForms from '@financial-times/o-forms';
-document.addEventListener('DOMContentLoaded', function() {
-	oForms.init();
-	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-});
